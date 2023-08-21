b347b86550d91d0480938a9e36b49fa49e2e0ea3
samirrathod9956 <61759700+samirrathod9956@users.noreply.github.com>
2023-08-21 10:31:47 +0530

    intial commit

---
+++ FieldEdgeWebDeveloper/Models/CustomerModel.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace FieldEdgeWebDeveloper.Models
+{
+    public class CustomerModel
+    {
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+
+        [JsonPropertyName("salutation")]
+        public string? Salutation { get; set; }
+
+        [JsonPropertyName("initials")]
+        public string? Initials { get; set; }
+
+        [JsonPropertyName("firstname")]
+        public string? Firstname { get; set; }
+
+        [JsonPropertyName("firstname_ascii")]
+        public string? Firstnameascii { get; set; }
+
+        [JsonPropertyName("gender")]
+        public string? Gender { get; set; }
+
+        [JsonPropertyName("firstname_country_rank")]
+        public string? FirstnameCountryRank { get; set; }
+
+        [JsonPropertyName("firstname_country_frequency")]
+        public string? FirstnameCountryFrequency { get; set; }
+
+        [JsonPropertyName("lastname")]
+        public string? Lastname { get; set; }
+
+        [JsonPropertyName("lastname_ascii")]
+        public string? Lastnameascii { get; set; }
+
+        [JsonPropertyName("lastname_country_rank")]
+        public string? LastnameCountryRank { get; set; }
+
+        [JsonPropertyName("lastname_country_frequency")]
+        public string? LastnameCountryFrequency { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [JsonPropertyName("email")]
+        public string? Email { get; set; }
+
+        [JsonPropertyName("password")]
+        public string? Password { get; set; }
+
+        [JsonPropertyName("country_code")]
+        public string? CountryCode { get; set; }
+
+        [JsonPropertyName("country_code_alpha")]
+        public string? CountryCodeAlpha { get; set; }
+
+        [JsonPropertyName("country_name")]
+        public string? CountryName { get; set; }
+
+        [JsonPropertyName("primary_language_code")]
+        public string? PrimaryLanguageCode { get; set; }
+
+        [JsonPropertyName("primary_language")]
+        public string? PrimaryLanguage { get; set; }
+
+        [JsonPropertyName("balance")]
+        public double? Balance { get; set; }
+
+        [JsonPropertyName("phone_Number")]
+        public string? PhoneNumber { get; set; }
+
+        [JsonPropertyName("currency")]
+        public string? Currency { get; set; }
+    }
+}
---
+++ FieldEdgeWebDeveloper/StudentClientService.cs
@@ -0,0 +1,45 @@
+﻿using FieldEdgeWebDeveloper.Models;
+
+namespace FieldEdgeWebDeveloper
+{
+    public class StudentClientService
+    {
+        private readonly HttpClient _httpClient;
+
+        public StudentClientService(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<List<CustomerModel>> GetStudentsAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<CustomerModel>>("Customers");
+        }
+
+        public async Task<CustomerModel> GetStudentsByIdAsync(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<CustomerModel>($"Customer/{id}");
+        }
+
+        public async Task<HttpResponseMessage> CreateStudentsAsync(CustomerModel customerModel)
+        {
+            var response = await _httpClient.PostAsJsonAsync("Customer", customerModel);
+            response.EnsureSuccessStatusCode();
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> UpdateStudentsAsync(string? id, CustomerModel? customerModel)
+        {
+            var response = await _httpClient.PostAsJsonAsync($"Customer/{id}", customerModel);
+            response.EnsureSuccessStatusCode();
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> DeleteStudentsAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"Customer/{id}");
+            response.EnsureSuccessStatusCode();
+            return response;
+        }
+    }
+}
